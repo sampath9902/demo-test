fe553ca979c6af4f8e2b528e5c7d0c1edad9f897
sampath9902 <147288614+sampath9902@users.noreply.github.com>
2024-06-28 09:38:13 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -4,8 +4,6 @@
 $projectPath = ".\super-service"
 $dockerImageName = "super-service-api"
 $dockerContainerName = "super-service-container"
-$dockerHubRepo = "yourdockerhubusername/$dockerImageName"
-$dockerTag = "latest"
 
 # Function to write log messages
 function Write-Log {
@@ -28,30 +26,17 @@ Write-Log "Building and packaging the application as a Docker image..."
 dotnet publish "$projectPath\SuperService.Api\SuperService.Api.csproj" -c Release -o "$projectPath\publish"
 
 # Docker build
-docker build -t $dockerImageName -f "$projectPath\Dockerfile" "$projectPath\publish"
+Write-Log "Building Docker image..."
+docker build -t $dockerImageName -f "$projectPath\Dockerfile" "$projectPath"
 if ($LASTEXITCODE -ne 0) {
     Write-Host "[ERROR] Docker build failed." -ForegroundColor Red
     exit $LASTEXITCODE
 }
 
-# 3. Push the Docker image to Docker Hub (optional step)
-# Uncomment the following lines if you want to push to Docker Hub
-# Write-Log "Pushing Docker image to Docker Hub..."
-# docker tag $dockerImageName $dockerHubRepo:$dockerTag
-# docker push $dockerHubRepo:$dockerTag
-# if ($LASTEXITCODE -ne 0) {
-#     Write-Host "[ERROR] Docker push failed." -ForegroundColor Red
-#     exit $LASTEXITCODE
-# }
-
-# 4. Deploy and run the image locally or in a public cloud
-
-# Stop and remove any existing container with the same name
-docker stop $dockerContainerName
-docker rm $dockerContainerName
-
-# Run the Docker container locally
+# 3. Deploy and run the image locally
 Write-Log "Deploying the Docker container locally..."
+docker stop $dockerContainerName 2>$null
+docker rm $dockerContainerName 2>$null
 docker run -d -p 5000:80 --name $dockerContainerName $dockerImageName
 if ($LASTEXITCODE -ne 0) {
     Write-Host "[ERROR] Docker run failed." -ForegroundColor Red
@@ -59,6 +44,7 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 Write-Log "Deployment successful. Application is running at http://localhost:5000"
+
 Dockerfile
 Create a Dockerfile in the super-service directory to package the .NET Core application.
 
